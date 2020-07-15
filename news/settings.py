f2fa19e189004f6c911be44d0943f34f80fc3583
konman1989 <manoilo.blog@gmail.com>
2020-07-15 20:43:54 +0300

    fix whitenoise

--- news/settings.py
+++ news/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = "users.User"
 
