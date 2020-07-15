c54276093cda41652d10ecdc08d28feb03236a47
konman1989 <manoilo.blog@gmail.com>
2020-07-15 20:40:34 +0300

    Revert "update whitenoise settings"
    This reverts commit a8a158892acfc0bc721d135a0f7b5b583f0ca3df.

--- news/settings/production.py
+++ news/settings/production.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = "users.User"
 
