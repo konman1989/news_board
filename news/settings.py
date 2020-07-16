f95b209c82981b3995f9c163785c1b8910ee0bd5
konman1989 <manoilo.blog@gmail.com>
2020-07-16 07:49:25 +0300

    fix settings

--- news/settings.py
+++ news/settings.py
@@ -135,9 +135,9 @@ STATIC_URL = "/static/"
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
