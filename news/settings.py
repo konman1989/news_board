907c72880ba555d33ed977e8d26ba0292ae451e3
konman1989 <manoilo.blog@gmail.com>
2020-07-16 07:57:40 +0300

    allowed hosts check

--- news/settings.py
+++ news/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(",")
+# ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(",")
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -135,9 +136,9 @@ STATIC_URL = "/static/"
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
